2ce7eb010ed4d590fc7fc3178567de6a46e949f9
Ihor Mizyk <ihm@INTERLOGIC.COM.UA>
2023-08-10 15:47:42 +0300

    added fetchThreads action

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -1,16 +1,18 @@
+"use server"
+
 import { revalidatePath } from "next/cache";
 import Thread from "../models/thread.model";
 import User from "../models/user.model";
 import { connectToDb } from "../mongoose";
 
-interface Params {
+interface CreateThreadParams {
   text: string;
   author: string;
   communityId: string | null;
   path: string;
 }
 
-export const createThread = async ({ text, author, communityId, path }: Params) => {
+export const createThread = async ({ text, author, communityId, path }: CreateThreadParams) => {
   try {
     connectToDb();
 
@@ -29,3 +31,44 @@ export const createThread = async ({ text, author, communityId, path }: Params) 
     throw new Error(`Couldnt create/update user: ${error.message}`);
   }
 };
+
+interface FetchThreadsParams {
+  page: number;
+  pageSize: number;
+}
+
+export const fetchThreads = async ({
+  page = 1,
+  pageSize = 10
+}: FetchThreadsParams) => {
+  try {
+    connectToDb();
+
+    const skip = (page - 1) * pageSize;
+
+    // threads without parents -> top level threads
+    const threadQuery = Thread.find({ parentId: { $exists: false } })
+      .sort({ createdAt: 'desc' })
+      .skip(skip)
+      .limit(pageSize)
+      .populate({ path: 'author', model: User })
+      .populate({
+        path: 'children',
+        populate: {
+          path: 'author',
+          model: User,
+          select: '_id name parentId image'
+        }
+      });
+
+    const totalCount = await Thread.countDocuments({ parentId: { $exists: false } })
+
+    const threads = await threadQuery.exec();
+
+    const isNext = totalCount > skip + threads.length;
+
+    return { threads, isNext };
+  } catch (error: any) {
+    throw new Error(`Couldnt create/update user: ${error.message}`);
+  }
+}
