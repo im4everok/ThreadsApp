040dce76c9e3bf65cda44a613a16a0cda0aaae47
Ihor Mizyk <ihm@INTERLOGIC.COM.UA>
2023-08-11 13:16:05 +0300

    Added thread page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,38 @@
-const Home = () => {
-    return (
-      <>
-        <h1 className='head-text text-left'>Home page</h1>
-      </>
-    )
+import ThreadCard from "@/components/cards/ThreadCard";
+import { fetchThreads } from "@/lib/actions/thread.actions";
+import { currentUser } from "@clerk/nextjs";
+
+const Home = async () => {
+  const result = await fetchThreads({ page: 1, pageSize: 10 });
+  const user = await currentUser();
+
+  return (
+    <>
+      <h1 className="head-text text-left">Home</h1>
+
+      <section className="mt-9 flex flex-col gap-10">
+        {result.threads.length === 0 ? (
+          <p className="no-result">No threads found</p>
+        ) : (
+          <>
+            {result.threads.map((thread) => (
+              <ThreadCard
+                key={thread._id}
+                id={thread._id}
+                currentUserId={user?.id || ""}
+                parentId={thread.parentId}
+                content={thread.text}
+                author={thread.author}
+                community={thread.community}
+                createdAt={thread.createdAt}
+                comments={thread.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
+    </>
+  )
 }
 
 export default Home;
