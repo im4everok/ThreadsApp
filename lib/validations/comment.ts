d0792262139d79b379a3d1b8f4b9a6fb63cd6df1
Ihor Mizyk <ihm@INTERLOGIC.COM.UA>
2023-08-11 13:11:21 +0300

    added comment validation

---
+++ lib/validations/comment.ts
@@ -0,0 +1,8 @@
+import * as z from "zod";
+
+export const CommentValidation = z.object({
+    thread: z.string().nonempty().min(3, {
+        message:
+            'Minimum 3 characters'
+    })
+});
