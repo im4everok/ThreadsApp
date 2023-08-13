216ec6d8a9a0eded0ed9d980e3405d64e9189561
im4everok <imizyk09@gmail.com>
2023-08-13 21:25:10 +0300

    centered auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,24 +1,28 @@
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
-import '../globals.css';
+import "../globals.css";
 
 export const metadata = {
-    title: 'Threads',
-    description: 'A next js 13 meta Threads app'
+  title: "Threads",
+  description: "A next js 13 meta Threads app",
 };
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
-export default function RootLayout({ 
-    children 
-}: { 
-    children: React.ReactNode
-}){
-    return <ClerkProvider>
-        <html lang="en">
-            <body className={`${inter.className} bg-dark-1`}>
-                {children}
-            </body>
-        </html>
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}>
+          <div className="w-full flex justify-center items-center">
+            {children}
+          </div>
+        </body>
+      </html>
     </ClerkProvider>
+  );
 }
