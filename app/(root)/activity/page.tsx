14a15dd4563879fa8c803fee34b4de49984bcd7e
im4everok <imizyk09@gmail.com>
2023-08-13 21:40:07 +0300

    added header to activity, added text with author's name and no activity message

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -14,35 +14,40 @@ const Page = async () => {
   if (!userInfo?.onboarded) redirect("/onboarding");
 
   const activity = await getActivity(userInfo._id);
-
+  
   return (
     <section>
-      <h1 className="head-text mb-10"></h1>
+      <h1 className="head-text mb-10">Activity</h1>
 
-    <section className="mt-10 flex flex-col gap-5">
-      {activity.length > 0 ? (
-        <>
-          {activity.map((act) => (
-            <Link id={act._id} href={`/thread/${act.parentId}`}>
-              <article className="activity-card">
-                <Image
-                src={act.author.image}
-                alt="Profile picture"
-                width={20}
-                height={20}
-                className="rounded-full object-cover"
-                />
-              </article>
-            </Link>
-          ))}
-        </>
-      ): (<>
-      </>)}
+      <section className="mt-10 flex flex-col gap-5">
+        {activity.length > 0 ? (
+          <>
+            {activity.map((act) => (
+              <Link id={act._id} href={`/thread/${act.parentId}`}>
+                <article className="activity-card">
+                  <Image
+                    src={act.author.image}
+                    alt="Profile picture"
+                    width={20}
+                    height={20}
+                    className="rounded-full object-cover"
+                  />
+                  <p className="!text-small-regular text-light-1">
+                    <span className="mr-1 text-primary-500">
+                      {act.author.name}
+                    </span>{" "}
+                    replied to your thread
+                  </p>
+                </article>
+              </Link>
+            ))}
+          </>
+        ) : (
+          <p className="!text-base-regular text-light-3">No activity yet</p>
+        )}
+      </section>
     </section>
-
-    </section>
-
-  )
-}
+  );
+};
 
 export default Page;
